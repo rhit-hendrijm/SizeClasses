cf1147155ebf3740dd478dae587bd22544a45c5b
Jacob Hendrich <hendrijm@rose-hulman.edu>
2020-12-16 17:32:33 -0500

    Initial Commit

---
+++ SizeClasses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SizeClasses
+//
+//  Created by Jacob Hendrich on 12/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
